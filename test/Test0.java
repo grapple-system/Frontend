09d9b69b449f3e4db4208c582150c06e1da0641f
Zhiqiang Zuo <sunzzq@gmail.com>
2017-11-27 20:32:54 -0800

    update

---
+++ test/Test0.java
@@ -0,0 +1,23 @@
+
+public class Test0 {
+//	int f;
+	
+	public Test0(){
+//		this.f = 1;
+	}
+
+	private void t1(int x, int y){
+		int a = x * x;
+		int c = a + x;
+		int b = y - c;
+//		int d = b + this.f;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
