504c5838618cc7908e00ed52a23d02dd8c56a7f9
Zhiqiang Zuo <sunzzq@gmail.com>
2017-11-30 21:08:37 -0800

    update

--- test/Test3.java
+++ test/Test3.java
@@ -1,12 +1,12 @@
 
-public class Test0 {
+public class Test3 {
 //	int f;
 	
-	public Test0(){
+	public Test3(){
 //		this.f = 1;
 	}
 
-	private void t1(int x, int y){
+	private void t1(int x, int y) throws Exception {
 		int a = x * x;
 		int c = a + x;
 		if(c > 0){
@@ -16,16 +16,32 @@ public class Test0 {
 			c--;
 		}
 		a++;
+		
+		if(a == 0)
+			throw new Exception("a is zero!");
+		
 		if(a < 0){
 			a--;
 		}
 		c++;
+		
 //		int d = b + this.f;
 	}
 	
 	
 	private void t2(int x){
 		int a = 19 / x;
+		try{
+			t1(x, a);
+			a++;
+			if(x == 0){
+				throw new Exception("divided by 0!");
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
